9d20a7bd90694829204b19fd29274252bfabf6ec
Brandi Abernathy <brandiabernathy22@gmail.com>
2024-05-09 18:53:51 -0700

    removed imagee

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
 
     return (
         <footer className="flex col-span-2 justify-center items-center mt-24">
-               Powered by <a className="grid" href="http://www.accuweather.com" rel="noreferrer" target="_blank"><img className="w-28 inline-block ml-2" src="./images/AccuWeather-logo.png" alt="AccuWeather"/></a>
+               Powered by&nbsp;<a className="grid" href="http://www.accuweather.com" rel="noreferrer" target="_blank">AccuWeather</a>
         </footer>
     )
 }
